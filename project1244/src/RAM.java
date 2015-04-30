f795f797018acdacc5bf8c704a144c4003befb53
RodMorris79 <rmorris@atl.yearup.org>
2015-04-30 11:03:39 -0400

    RAM.java

---
+++ project1244/src/RAM.java
@@ -0,0 +1,9 @@
+
+public class RAM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("I have two brothers with the same initals as me."); 
+	}
+
+}
