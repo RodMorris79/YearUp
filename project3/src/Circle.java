93a39b5ced0d98af5fec107c45320f27a45e0c77
RodMorris79 <rmorris@atl.yearup.org>
2015-05-08 07:59:53 -0400

    Circle.file

---
+++ project3/src/Circle.java
@@ -0,0 +1,43 @@
+
+public class Circle {
+	{
+	String m_color;
+	double m_radius =2.0;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+		public Circle (double m_radius, string m_color;
+		m_radius= new m_radius1;
+		m_color="grey";
+		
+		System.out.println ("the radius is " + m_radius_1, "The color is" + m_color);
+		
+		public Circle (double m_radius, string m_color;
+
+		m_radius = new m_radius2;
+		m_color	= new m_color1;
+		}
+		static double CalculateArea(double radius)
+		{
+			double area =3.14*radius*radius;
+			return area;
+			
+			{
+				public static void (String [] args):
+					System.out.println ("my color is" + m_color)
+				
+			}
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+}
---
+++ project3/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	public static void main(String[] args)
+int m_radius = 5;
+int m radius1 = 10;
+	m_color
+{
+	cirlce= new bigCircle (m_radius1);
+	m_radius1=10;
+}
+	circle =new smallCircle (m_radius, m_color)
+	m_radius= 5
+	m_color= new m_color;
+}
+
+
